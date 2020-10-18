0738681e4baa7d03df0e590c390660934cb8be87
kai14335 <kai.portalatin@gmail.com>
2020-10-18 15:10:13 -0400

    added header + meaning of some lines of code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FlyWheelTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FlyWheelTest.java
@@ -1,20 +1,40 @@
+/*
+//FTC FROGS (#14335) TEAM CODE
+//
+//  Class:
+//      FlyWheelTest
+//
+//  Methods:
+//      wheelTrigger
+//
+//
+//  Revisions:
+//      10/18/20    Original    Kai P + Elijah E: Original
+ */
+
+
+
+
+
+
+
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 public class FlyWheelTest {
-
+//declare members
     private DcMotor flywheel1;
     private LinearOpMode opMode;
-
+//sets name of variable, dependent on the pressure placed on the trigger
     private double speed;
 
     public FlyWheelTest (LinearOpMode opmode) {
         this.opMode = opmode;
         flywheel1 = opMode.hardwareMap.get(DcMotor.class,"launcher");
     }
-
+//sets the gamepad button trigger for the fly wheel
     public void wheelTrigger(){
         speed = opMode.gamepad1.right_trigger;
         flywheel1.setPower(speed);
