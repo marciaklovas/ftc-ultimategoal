5f7a287cb10224a2a35fc378f82dffdaa8639c12
marciaklovas <marcia.k.lovas@gmail.com>
2020-12-06 14:59:16 -0500

    Add Launcher code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Launcher.java
@@ -0,0 +1,49 @@
+package org.firstinspires.ftc.teamcode;
+
+/*
+//FTC FROGS (#14335) TEAM CODE
+//
+//  Class:
+//      Launcher
+//
+//  Methods:
+//      Launcher
+//
+//
+//  Revisions:
+//      12/6/20    Original    Kai P + Elijah E: Original
+ */
+
+
+
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class Launcher {
+
+    private LinearOpMode opMode;
+    private DcMotor launcherMotor;
+    private double speed2;
+    private boolean buttonspeed2;
+
+
+    private Launcher (LinearOpMode opmode) {
+        this.opMode = opmode;
+        launcherMotor = opMode.hardwareMap.get(DcMotor.class,"launcher");
+        launcherMotor.setDirection(DcMotor.Direction.FORWARD);
+
+    }
+
+    public void wheelTrigger() {
+        speed2 = opMode.gamepad1.right_trigger;
+        launcherMotor.setPower(speed2);
+    }
+
+    public void wheelButton() {
+        buttonspeed2 = opMode.gamepad1.right_bumper;
+        //flywheel1.setPower(speed);
+        launcherMotor.setPower(1);
+    }
+
+}
