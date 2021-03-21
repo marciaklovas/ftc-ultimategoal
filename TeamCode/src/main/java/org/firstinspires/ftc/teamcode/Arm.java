afb4b04365584c6230ebd120b8def7840a42100f
kai14335 <kai.portalatin@gmail.com>
2021-03-21 13:41:41 -0400

    added a minus to telemetry

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Arm.java
@@ -48,7 +48,7 @@ public class Arm {
 
     public void adjustArm()
     {
-        arm.setPosition((opmode.gamepad2.right_stick_y/2)+.5);
+        arm.setPosition((-opmode.gamepad2.right_stick_y/2)+.5);
 
         opmode.telemetry.addData("armposition: ",
                 arm.getPosition());
